9dd0be314d3a73adcf536159797d7221a8781df5
JulienDuchow <duchow.julien@gmail.com>
2025-01-18 10:03:06 +0100

    websupport try#4

--- lib/database/web.dart
+++ lib/database/web.dart
@@ -6,5 +6,25 @@ import 'package:sqlite3/wasm.dart';
 import '../InnerDatabase.dart';
 
 Future<InnerDatabase> constructDb(String name, int dbVersion, MigrationStrategy migrationStrategy, {bool logStatements = false, bool useOldPath = false}) async {
-  return InnerDatabase(WasmDatabase(path: name,sqlite3: await WasmSqlite3.loadFromUrl(Uri.parse('sqlite3.wasm')),  logStatements: logStatements), dbVersion, migrationStrategy);
+ // return InnerDatabase(WasmDatabase(path: name,sqlite3: await WasmSqlite3.loadFromUrl(Uri.parse('sqlite3.wasm')),  logStatements: logStatements), dbVersion, migrationStrategy);
+
+
+  final result = await WasmDatabase.open(
+    databaseName: name, // prefer to only use valid identifiers here
+    sqlite3Uri: Uri.parse('sqlite3.wasm'),
+    driftWorkerUri: Uri.parse('drift_worker.dart.js'),
+  );
+
+  if (result.missingFeatures.isNotEmpty) {
+    // Depending how central local persistence is to your app, you may want
+    // to show a warning to the user if only unrealiable implemetentations
+    // are available.
+    print('Using ${result.chosenImplementation} due to missing browser '
+        'features: ${result.missingFeatures}');
+  }
+
+
+
+  return InnerDatabase(result.resolvedExecutor.executor, dbVersion, migrationStrategy);
+
 }
